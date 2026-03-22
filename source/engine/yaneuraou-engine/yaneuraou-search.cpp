f65153bae658660af80ee05ea100a3a9adc2ad70
KazApps <kazuki_shogi@outlook.jp>
2026-03-23 00:03:42 +0900

    increase reduction in singular extension search

--- source/engine/yaneuraou-engine/yaneuraou-search.cpp
+++ source/engine/yaneuraou-engine/yaneuraou-search.cpp
@@ -3538,6 +3538,10 @@ moves_loop:  // When in check, search starts here
 
         r -= ss->statScore * 794 / 8192;
 
+        // Singular Extension Searchであるならreductionを増やす
+        if (excludedMove)
+            r += 768;
+
 		// -----------------------
         // Step 17. Late moves reduction / extension (LMR)
         // Step 17. 遅い指し手の削減／延長（LMR)
